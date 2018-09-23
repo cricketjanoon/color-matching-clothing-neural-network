66fc4a3de7bec4290a65210d7e55ce72ae4f8055
Shahbaz Ali <cricketjanoon1111@gmail.com>
2018-09-23 20:10:31 +0500

    Final editing.

--- displayDataSingle.m
+++ displayDataSingle.m
@@ -1,10 +1,3 @@
 function [] = displayDataSingle(X, map)
-%DISPLAYDATA Display 2D data in a nice grid
-%   [h, display_array] = DISPLAYDATA(X, example_width) displays 2D data
-%   stored in X in a nice grid. It returns the figure handle h and the 
-%   displayed array if requested.
-
-
-
 imshow(reshape(X,100,100),map);
 end
